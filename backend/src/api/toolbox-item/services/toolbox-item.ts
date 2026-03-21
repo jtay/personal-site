82c7dfc3b6c7bcfdd5560e4c42d5df5eb3dad1b5
Jaydon Taylor <jaydon.taylor@onstate.co.uk>
2026-03-21 15:05:24 +0000

    fix: Add `as any` type assertion to Strapi core factory calls.

--- backend/src/api/toolbox-item/services/toolbox-item.ts
+++ backend/src/api/toolbox-item/services/toolbox-item.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::toolbox-item.toolbox-item');
+export default factories.createCoreService('api::toolbox-item.toolbox-item' as any);
